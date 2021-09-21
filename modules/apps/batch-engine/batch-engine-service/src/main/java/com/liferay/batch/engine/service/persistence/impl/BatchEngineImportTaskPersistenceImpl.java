529fefbb2cc72e0550dc5ce2fd57d902f7f5eb03
joemduffy <89780329+joemduffy@users.noreply.github.com>
2021-09-21 10:54:43 +0100

    COMMERCE-7146 batch-engine-service autogenerated

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/service/persistence/impl/BatchEngineImportTaskPersistenceImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/service/persistence/impl/BatchEngineImportTaskPersistenceImpl.java
@@ -31,13 +31,19 @@ import com.liferay.portal.kernel.dao.orm.QueryPos;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.dao.orm.SessionFactory;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.sanitizer.Sanitizer;
+import com.liferay.portal.kernel.sanitizer.SanitizerException;
+import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.persistence.BasePersistence;
 import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -2559,6 +2565,33 @@ public class BatchEngineImportTaskPersistenceImpl
 			else {
 				batchEngineImportTask.setModifiedDate(
 					serviceContext.getModifiedDate(date));
+			}
+		}
+
+		long userId = GetterUtil.getLong(PrincipalThreadLocal.getName());
+
+		if (userId > 0) {
+			long companyId = batchEngineImportTask.getCompanyId();
+
+			long groupId = 0;
+
+			long batchEngineImportTaskId = 0;
+
+			if (!isNew) {
+				batchEngineImportTaskId = batchEngineImportTask.getPrimaryKey();
+			}
+
+			try {
+				batchEngineImportTask.setErrorMessage(
+					SanitizerUtil.sanitize(
+						companyId, groupId, userId,
+						BatchEngineImportTask.class.getName(),
+						batchEngineImportTaskId, ContentTypes.TEXT_PLAIN,
+						Sanitizer.MODE_ALL,
+						batchEngineImportTask.getErrorMessage(), null));
+			}
+			catch (SanitizerException sanitizerException) {
+				throw new SystemException(sanitizerException);
 			}
 		}
 
