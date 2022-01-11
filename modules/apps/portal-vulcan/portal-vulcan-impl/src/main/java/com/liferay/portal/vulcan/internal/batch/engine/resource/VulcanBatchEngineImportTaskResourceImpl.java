ae2fde4965ab51299867d71134e2a808e7b97f34
joemduffy <89780329+joemduffy@users.noreply.github.com>
2022-01-11 10:36:09 +0000

    COMMERCE-7988 portal-vulcan-impl java changes for new parameters

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/batch/engine/resource/VulcanBatchEngineImportTaskResourceImpl.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/batch/engine/resource/VulcanBatchEngineImportTaskResourceImpl.java
@@ -58,7 +58,8 @@ public class VulcanBatchEngineImportTaskResourceImpl
 		_initializeContext();
 
 		return _importTaskResource.postImportTask(
-			name, callbackURL, fields, _getTaskItemDelegateName(), object);
+			name, callbackURL, null, null, fields, _getTaskItemDelegateName(),
+			object);
 	}
 
 	@Override
